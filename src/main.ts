df5350f4acc41bd8777bd740f848100ddd93bdfd
Hosam Tarade <tardehosam@gmail.com>
2026-09-01 12:17:53 -0700

    Add Swagger API documentation at /api-docs

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/http-exception.filter';
@@ -17,6 +18,18 @@ async function bootstrap() {
   );
 
   app.useGlobalFilters(new AllExceptionsFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('Inventory Management System API')
+    .setDescription(
+      'نظام إدارة مخزون موزّع متعدد الفروع - توثيق كامل لكل الـ Endpoints',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
